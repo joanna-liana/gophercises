ea86488353c680fd020f8ad6ac5f268c25e6f85e
Joanna Liana <joanna_liana@outlook.com>
2019-12-27 18:27:17 +0100

    Add results info + parse user settings (flags)

--- 01_csv_quiz/quiz.go
+++ 01_csv_quiz/quiz.go
@@ -9,22 +9,64 @@ import (
     "io"
     "encoding/csv"
     "strings"
+    "strconv"
 )
 
-func main() {
-    defaultFile := "questions.csv"
+type WrongAnswerInfo struct {
+    Question string
+    PlayerAnswer string
+    CorrectAnswer string
+}
 
-    csvFileFlag := flag.String("csv", defaultFile, "A CSV file in the format: question, answer")
-    timeTimitFlag := flag.Int("limit", 30, "The time limit for answering a single question")
+type ProgramDefaults struct {
+    DefaultFile string
+    DefaultTimeLimit int
+}
+
+func getDefaults() ProgramDefaults {
+    defaultFile := "questions.csv"
+    defaultTimeLimit := 30
+
+    setDefaults := func (f *flag.Flag) {
+        switch f.Name {
+            case "csv" :
+                defaultFile = f.Value.String()
+
+            case "limit":
+                intLimit, err := strconv.Atoi(f.Value.String())
+                if err != nil {
+                    log.Fatal(err)
+                    return
+                }
+                defaultTimeLimit = intLimit
+        }
+    }
+
+    flag.String("csv", defaultFile, "A CSV file in the format: question, answer")
+    flag.Int("limit", defaultTimeLimit, "The time limit for answering a single question")
     flag.Parse()
 
-    fmt.Println(csvFileFlag)
-    fmt.Println(timeTimitFlag)
+    flag.Visit(setDefaults)
+
+    return ProgramDefaults {
+        DefaultFile: defaultFile,
+        DefaultTimeLimit: defaultTimeLimit,
+     }
+}
+
+func main() {
+    defaults := getDefaults()
+    defaultFile, defaultTimeLimit := defaults.DefaultFile, defaults.DefaultTimeLimit
+
+    fmt.Println("Time limit:", defaultTimeLimit)
 
     csvBuffer, err := os.Open(defaultFile)
     if err != nil {
         log.Fatal(err)
     }
+
+    questionsCount := 0
+    var wrongAnswersInfo []WrongAnswerInfo
 
     cvsReader := csv.NewReader(bufio.NewReader(csvBuffer))
     for {
@@ -36,21 +78,38 @@ func main() {
             log.Fatal(error)
         }
 
+        questionsCount += 1
         question := line[0]
-        answer := line[1]
+        answer := strings.TrimSpace(line[1])
 
         buf := bufio.NewReader(os.Stdin)
         fmt.Print(question, "> ")
         userAnswer, err := buf.ReadString('\n')
+        parsedUserAnswer := strings.TrimSpace(userAnswer)
 
         if err != nil {
             fmt.Println(err)
         } else {
-            if strings.TrimSpace(userAnswer) == strings.TrimSpace(answer) {
-                fmt.Println("Well done!")
+            if  parsedUserAnswer == answer {
+                fmt.Print("Well done!\n\n")
             } else {
-                fmt.Println("Wrong answer")
+                fmt.Print("Wrong answer\n\n")
+                answerInfo := WrongAnswerInfo{ Question: question, PlayerAnswer: parsedUserAnswer, CorrectAnswer: answer }
+                wrongAnswersInfo = append(wrongAnswersInfo, answerInfo)
             }
+        }
+    }
+
+    wrongAnswersCount := len(wrongAnswersInfo)
+
+    fmt.Println("Final score:", questionsCount - wrongAnswersCount, "/", questionsCount)
+
+    if wrongAnswersCount > 0 {
+        fmt.Print("\nWrong answers - details\n\n")
+        for _, info := range(wrongAnswersInfo) {
+            fmt.Println("Question:", info.Question)
+            fmt.Println("Your answer:", info.PlayerAnswer)
+            fmt.Print("Correct answer:", info.CorrectAnswer, "\n\n")
         }
     }
 }
